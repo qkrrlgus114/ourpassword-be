9e3c5f69b5dbf061e338e5e2e3ca8c998a550554
qkrrlgus114 <qkrrlgus114@naver.com>
2024-12-09 21:37:10 +0900

    feat : 초기 설정 진행
    - 시큐리티 기본 설정 진행
    
    - yml 이그노어 추가

---
+++ ourpassword/src/main/java/com/park/ourpassword/OurpasswordApplication.java
@@ -0,0 +1,13 @@
+package com.park.ourpassword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurpasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurpasswordApplication.class, args);
+	}
+
+}
---
+++ ourpassword/src/test/java/com/park/ourpassword/OurpasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.ourpassword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurpasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
