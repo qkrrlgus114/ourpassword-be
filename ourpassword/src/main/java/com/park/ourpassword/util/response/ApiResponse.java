8b395e1859217baa09bbb669443bf27677f6586a
박기현 <qkrrlgus114@naver.com>
2024-12-28 17:10:15 +0900

    refactor : ApiResponse에 제네릭 적용

--- ourpassword/src/main/java/com/park/ourpassword/util/response/ApiResponse.java
+++ ourpassword/src/main/java/com/park/ourpassword/util/response/ApiResponse.java
@@ -1,69 +1,58 @@
 package com.park.ourpassword.util.response;
 
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Builder
-public class ApiResponse {
+public class ApiResponse<T> {
 
-	private String status;
-	private Object data;
-	private String message;
+    private String status;
+    private T data;
+    private String message;
 
-	private static final String SUCCESS = "success";
-	private static final String FAIL = "fail";
-	private static final String ERROR = "error";
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+    private static final String ERROR = "error";
 
-	public ApiResponse(String status, Object data, String message) {
-		this.status = status;
-		this.data = data;
-		this.message = message;
-	}
+    public ApiResponse(String status, T data, String message) {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+    }
 
-	public ApiResponse(String status, String message) {
-		this.status = status;
-		this.message = message;
-	}
+    public ApiResponse(String status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 
-	/**
-	 * 성공(상태 + 데이터 + 메시지)
-	 */
-	public static ApiResponse successDataMessage(Object data, String message) {
-		return ApiResponse.builder()
-			.status(SUCCESS)
-			.data(data)
-			.message(message).build();
-	}
+    /**
+     * 성공(상태 + 데이터 + 메시지)
+     */
+    public static <T> ApiResponse<T> successDataMessage(T data, String message) {
+        return new ApiResponse<>(SUCCESS, data, message);
+    }
 
-	/**
-	 * 성공(상태 + 메시지)
-	 */
-	public static ApiResponse successMessage(String message) {
-		return ApiResponse.builder()
-			.status(SUCCESS)
-			.message(message).build();
-	}
+    /**
+     * 성공(상태 + 메시지)
+     */
+    public static ApiResponse successMessage(String message) {
+        return new ApiResponse(SUCCESS, message);
+    }
 
-	/**
-	 * 실패(상태 + 메시지)
-	 */
-	public static ApiResponse failMessage(String message) {
-		return ApiResponse.builder()
-			.status(FAIL)
-			.message(message).build();
-	}
+    /**
+     * 실패(상태 + 메시지)
+     */
+    public static ApiResponse failMessage(String message) {
+        return new ApiResponse(FAIL, message);
+    }
 
-	/**
-	 * 에러(상태 + 메시지)
-	 */
-	public static ApiResponse errorMessage(String message) {
-		return ApiResponse.builder()
-			.status(ERROR)
-			.message(message).build();
-	}
+    /**
+     * 에러(상태 + 메시지)
+     */
+    public static ApiResponse errorMessage(String message) {
+        return new ApiResponse(ERROR, message);
+    }
 
 }
