0cd21a3f961167da70eb5ce9c6317411fd23113e
DESKTOP-4DM5HI6\qkrrl <qkrrlgus114@naver.com>
2024-12-30 01:19:48 +0900

    test : ci 테스트

--- ourpassword/src/main/java/com/park/ourpassword/domain/exception/GlobalExceptionHandler.java
+++ ourpassword/src/main/java/com/park/ourpassword/domain/exception/GlobalExceptionHandler.java
@@ -1,21 +1,19 @@
 package com.park.ourpassword.domain.exception;
 
+import com.park.ourpassword.util.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.park.ourpassword.util.response.ApiResponse;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
 @Slf4j
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(CommonException.class)
-	public ResponseEntity<ApiResponse> getException(BaseException e) {
-		return ResponseEntity.status(e.getStatus()).body(ApiResponse.errorMessage(e.getExceptionMessage()));
-	}
+    @ExceptionHandler(CommonException.class)
+    public ResponseEntity<ApiResponse<?>> getException(BaseException e) {
+        return ResponseEntity.status(e.getStatus()).body(ApiResponse.errorMessage(e.getExceptionMessage()));
+    }
 }
